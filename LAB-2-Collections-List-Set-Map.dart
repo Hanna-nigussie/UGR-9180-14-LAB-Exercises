df181a821e87d9be8f3e8d638cdb4421fc175adb
hannanigussie <hniguse95@gmail.com>
2024-03-05 16:31:33 +0300

    Exercise 2 collections-list-map

---
+++ LAB-2-Collections-List-Set-Map.dart
@@ -0,0 +1,8 @@
+void printMap(Map<dynamic, dynamic> map) {
+  map.forEach((key, value) => print('Key: $key, Value: $value'));
+}
+
+void main() {
+  Map<String, int> myMap = {'a': 1, 'b': 2, 'c': 3};
+  printMap(myMap);
+}
